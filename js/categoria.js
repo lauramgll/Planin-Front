5f253720d6280ce88a93bc1e0bb859700e52dd0f
lauramgll <156073035+lauramgll@users.noreply.github.com>
2024-05-26 14:14:36 +0200

    Funcionalidad elegir categorías

---
+++ js/categoria.js
@@ -0,0 +1,20 @@
+'use strict'
+
+import { cargarMenu, checkUser } from './utils.js';
+
+window.addEventListener("DOMContentLoaded", () => {
+    checkUser();
+    cargarMenu();
+
+    const categorias = document.querySelectorAll('#vistaCategorias div');
+    categorias.forEach(function(categoria) {
+        categoria.addEventListener('click', function() {
+            let cat = categoria.getAttribute("id").split("_");
+            let idCat = cat[cat.length - 1];
+            let nombreCat = cat[0];
+            localStorage.setItem('idCategoria', idCat);
+            localStorage.setItem('nombreCategoria', nombreCat);
+            history.back();
+        });
+    });
+})
