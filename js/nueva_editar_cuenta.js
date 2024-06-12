cca135fea9644ba582d66bd6e35b788606bbcbd7
lauramgll <156073035+lauramgll@users.noreply.github.com>
2024-06-12 12:19:10 +0200

    Arreglo mostrar importe cuenta seleccionada

--- js/nueva_editar_cuenta.js
+++ js/nueva_editar_cuenta.js
@@ -35,7 +35,11 @@ window.addEventListener("DOMContentLoaded", () => {
             btnBorrar.style.display = "block";
             txtSaldo.textContent = "Saldo";
             nombre.value = JSON.parse(localStorage.getItem("cuentaSeleccionada")).nombre;
-            numeroInput.value = (JSON.parse(localStorage.getItem("cuentaSeleccionada")).saldo).toFixed(2).toString().replace('.', ',');
+            if(localStorage.getItem("cuentaSeleccionada").saldo == null) {
+                numeroInput.value = "0,00";
+            } else {
+                numeroInput.value = (JSON.parse(localStorage.getItem("cuentaSeleccionada")).saldo).toFixed(2).toString().replace('.', ',');
+            }
             predeterminada.textContent = JSON.parse(localStorage.getItem("cuentaSeleccionada")).predeterminada;
 
             if (predeterminada.textContent == "Sí") {
