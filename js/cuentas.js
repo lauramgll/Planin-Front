f0d044833b6c76e8560855c6661608a6cdc12ffe
lauramgll <156073035+lauramgll@users.noreply.github.com>
2024-06-12 12:40:30 +0200

    .

--- js/cuentas.js
+++ js/cuentas.js
@@ -46,6 +46,10 @@ export async function actualizarSaldoCuentas(cuentas) {
 
         cuenta.saldo = saldoCuenta;
 
+        if(cuenta.saldo == null) {
+            cuenta.saldo = 0;
+        }
+
         await fetch(`${URL}/cuentas/${cuenta.id}`, {
             method: 'PUT',
             headers: {
